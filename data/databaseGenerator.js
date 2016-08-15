aed9f002d22ec781f5ea9596bbced95702635f4d
Alex Schmoe <aschmoe@gmail.com>
2016-08-15 10:09:51 -0700

    catlies inital commit

--- data/databaseGenerator.js
+++ data/databaseGenerator.js
@@ -1,14 +1,14 @@
 'use strict';
 
 /**
- * Command line script that generates a SQLite database file that contains jokes about Chuck Norris using the
- * wonderful http://api.icndb.com/jokes APIs
+ * Command line script that generates a SQLite database file that contains lies about Chuck Norris using the
+ * wonderful http://api.icndb.com/lies APIs
  *
  * Usage:
  *
  *   node databaseGenerator.js [destFile]
  *
- *   destFile is optional and it will default to "norrisbot.db"
+ *   destFile is optional and it will default to "catliesbot.db"
  *
  * @author Luciano Mammino <lucianomammino@gmail.com>
  */
@@ -19,10 +19,10 @@ var Async = require('async');
 var ProgressBar = require('progress');
 var sqlite3 = require('sqlite3').verbose();
 
-var outputFile = process.argv[2] || path.resolve(__dirname, 'norrisbot.db');
+var outputFile = process.argv[2] || path.resolve(__dirname, 'catliesbot.db');
 var db = new sqlite3.Database(outputFile);
 
-// executes an API request to count all the available jokes
+// executes an API request to count all the available lies
 request('http://api.icndb.com/jokes/count', function (error, response, body) {
     if (!error && response.statusCode === 200) {
         var count = JSON.parse(body).value;
@@ -34,20 +34,20 @@ request('http://api.icndb.com/jokes/count', function (error, response, body) {
         db.serialize();
         // Creates the database structure
         db.run('CREATE TABLE IF NOT EXISTS info (name TEXT PRIMARY KEY, val TEXT DEFAULT NULL)');
-        db.run('CREATE TABLE IF NOT EXISTS jokes (id INTEGER PRIMARY KEY, joke TEXT, used INTEGER DEFAULT 0)');
-        db.run('CREATE INDEX jokes_used_idx ON jokes (used)');
+        db.run('CREATE TABLE IF NOT EXISTS lies (id INTEGER PRIMARY KEY, lie TEXT, used INTEGER DEFAULT 0)');
+        db.run('CREATE INDEX lies_used_idx ON lies (used)');
 
-        // The idea from now on is to iterate through all the possible jokes starting from the index 1 until we can
+        // The idea from now on is to iterate through all the possible lies starting from the index 1 until we can
         // find all the available ones. There might be holes in the sequence, so we might want to issue all the request
-        // sequentially and count the successful requests until we get the total amount of jokes.
+        // sequentially and count the successful requests until we get the total amount of lies.
         // We are going to use the function Async.whilst so we need to define 3 functions: test, task and onComplete
 
-        // Tests whether to stop fetching jokes. It gets called before starting a new iteration
+        // Tests whether to stop fetching lies. It gets called before starting a new iteration
         var test = function () {
             return savedJokes < count;
         };
 
-        // The task executed at every iteration. Basically fetches a new joke and creates a new record in the database.
+        // The task executed at every iteration. Basically fetches a new lie and creates a new record in the database.
         var task = function (cb) {
             request('http://api.icndb.com/jokes/' + (++index) + '?escape=javascript', function (err, response, body) {
                 // handle possible request errors by stopping the whole process
@@ -63,11 +63,25 @@ request('http://api.icndb.com/jokes/count', function (error, response, body) {
                 var result = null;
                 try {
                     result = JSON.parse(body).value;
+                    result.joke = result.joke.replace(/The Chuck\ Norris/g, 'The cat');
+                    result.joke = result.joke.replace(/the Chuck\ Norris/g, 'the cat');
+                    result.joke = result.joke.replace(/\.\ Chuck\ Norris/g, '. A cat');
+                    result.joke = result.joke.replace(/^Chuck\ Norris/g, 'A cat');
+                    result.joke = result.joke.replace(/Chuck\ Norris/g, 'a cat');
+                    result.joke = result.joke.replace(/the Chuck/g, 'the cat');
+                    result.joke = result.joke.replace(/Chuck/g, 'the cat');
+                    result.joke = result.joke.replace(/Norris/g, 'the cat');
+                    result.joke = result.joke.replace(/Walker/g, 'Kitty');
+                    result.joke = result.joke.replace(/Texas\ Ranger/g, 'Bird Raper');
+                    result.joke = result.joke.replace(/finger/g, 'claw');
+                    result.joke = result.joke.replace(/fist/g, 'paw');
+                    result.joke = result.joke.replace(/cat'\ /g, 'cat\'s ');
+                    console.log(result.joke);
                 } catch (ex) {
                     return cb(null);
                 }
 
-                db.run('INSERT INTO jokes (joke) VALUES (?)', result.joke, function (err) {
+                db.run('INSERT INTO lies (lie) VALUES (?)', result.joke, function (err) {
                     if (err) {
                         return cb(err);
                     }
@@ -92,6 +106,6 @@ request('http://api.icndb.com/jokes/count', function (error, response, body) {
         return Async.whilst(test, task, onComplete);
     }
 
-    console.log('Error: unable to count the total number of jokes');
+    console.log('Error: unable to count the total number of lies');
     process.exit(1);
 });
